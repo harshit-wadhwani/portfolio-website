d44e890a0666ca6ad330c9165f25ef068ae0860c
harshit <harshitwadhwani23@gmail.com>
2023-03-15 18:20:53 +0530

    added requirements.txt

--- linkopen.py
+++
@@ -1,8 +0,0 @@
-from bokeh.models.widgets import Div
-
-def open_link(link):
-    js = f"window.open({link})"  # New tab or window
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    return div
-    
