3034a3e789426f4742d60da8c472b863db6ae080
harshit <harshitwadhwani23@gmail.com>
2023-03-15 05:49:53 +0530

    added educational qualificiations temporary

--- app.py
+++ app.py
@@ -33,6 +33,87 @@ with col3:
     st.button("Streamlit")
     st.button('NumPy')
 
-jsonex = '{}'
+jsonex = {'title' : {'media' : {
+    'url' : 'https://img.freepik.com/free-photo/front-view-stacked-books-graduation-cap-diploma-education-day_23-2149241011.jpg?w=2000',
+    'caption' : "",
+    'credit' : ''
+         },
+         'text' : {
+             'headline' : 'Education',
+             'text' : '<p>Educational Background</p>'
+         }},
+          'events':[
+               {
+        "media": {
+          "url": r"https://upload.wikimedia.org/wikipedia/en/4/44/Ryan_International_Group_logo.png",
+          "caption": "<p>      Ryan Internaltional School</p>"
+        },
+        "start_date": {
+          "year": "2008"
+        },
+        "text": {
+          "headline": "Ryan International School, Surat",
+          "text": "Studied in Ryan International School From Class 1st to 6th, after which I shifted to another city."
+        }
+      },
+                  {
+        "media": {
+          "url": r"https://play-lh.googleusercontent.com/E4ZfTtsuisAiM9H7jVGci-wmLN6o-xhCXIM6BwOvtrwP-FEFu6RDSewxPDu_5Z5-ig",
+          "caption": "<p>      St. Aloysius sr. sec. School</p>"
+        },
+        "start_date": {
+          "year": "2015"
+        },
+        "text": {
+          "headline": "St. Aloysius sr. sec. School, Jabalpur",
+          "text": "Studied in St. Aloysius sr. sec. School for Class 7th, after which I shifted to surat again."
+        }
+      },
+        {
+        "media": {
+          "url": r"https://upload.wikimedia.org/wikipedia/en/4/44/Ryan_International_Group_logo.png",
+          "caption": "<p>      Ryan Internaltional School</p>"
+        },
+        "start_date": {
+          "year": "2018"
+        },
+        "text": {
+          "headline": "Ryan International School, Surat",
+          "text": "Studied in Ryan International School For Class 10th and Scored 78.8%"
+        }
+      },
+        {
+        "media": {
+          "url": r"https://upload.wikimedia.org/wikipedia/en/4/44/Ryan_International_Group_logo.png",
+          "caption": "<p>      Ryan Internaltional School</p>"
+        },
+        "start_date": {
+          "year": "2020"
+        },
+        "text": {
+          "headline": "Ryan International School, Surat",
+          "text": "Studied in Ryan International School For Class 12th and Scored 88%"
+        }
+      }, 
+        {
+        "media": {
+          "url": r"https://img.collegedekhocdn.com/media/img/institute/logo/dsi_logo.png",
+          "caption": "<p>      Dayananda Sagar Academy of Technology and Management</p>"
+        },
+        "start_date": {
+          "year": "2020"
+        },
+        "text": {
+          "headline": "DSATM, Bengaluru",
+          "text": "Pursuing Bachelors of engineering in Artificial Intelligence and Machine Learning"
+        }
+      }
+                  
+          ]
+          }
 
 st.title('Education')
+timeline(jsonex, height=500 )
+
+
+st.title("Certifications")
